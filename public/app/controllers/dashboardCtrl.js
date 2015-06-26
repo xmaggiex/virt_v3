7643bd8d5397e9b73e46e5c846059ee60d5b0beb
Piotrrekkk <piotrek0rosa@gmail.com>
2015-06-26 16:15:54 +0200

    dashboard working

--- public/app/controllers/dashboardCtrl.js
+++ public/app/controllers/dashboardCtrl.js
@@ -4,20 +4,61 @@
 * Description
 */
 angular.module('dashboardController', ['ComputersService'])
-	.controller('dashboardCtrl', function(Computers){
+	.controller('dashboardCtrl', function($scope, Computers){
 		var vm = this;
 
-		Computers.all().success(function(data) {
-			vm.computers = data.computers;
-		})
+		getData();
 
 		vm.updateStatusRow = function(computer, virt) {
-			vm.machine = {};
+			if(!vm.machine) {
+				vm.machine = {};	
+			}
 			this.computer = computer || 0;
 			this.virt = virt || 0;
 			if(virt != '') {
-				vm.machine = vm.computers[this.computer].virtuals[this.virt];
+				var index = 2;
+				vm.machine[this.computer] = vm.computers[this.computer].virtuals[this.virt];
+			}
+			else {
+				vm.machine[this.computer] = {};
 			}
 		}
 
+		vm.makeSnapshot = function(machine, ip) {
+			if(machine) {
+				console.log("Push snapshot request");
+				console.log("machine Name: " + machine.name);
+				console.log("IP Physical machine: " + ip);
+			}
+		}
+		vm.clone = function(machine, ip) {
+			if(machine) {
+				console.log("Push clone request");
+				console.log("machine Name: " + machine.name);
+				console.log("IP Physical machine: " + ip);
+			}
+		}
+		vm.startVirt = function(machine, ip) {
+			if(machine) {
+				console.log("Push startVirt request");
+				console.log("machine Name: " + machine.name);
+				console.log("IP Physical machine: " + ip);
+			}
+		}
+		vm.stopVirt = function(machine, ip) {
+			if(machine) {
+				console.log("Push stopVirt request");
+				console.log("machine Name: " + machine.name);
+				console.log("IP Physical machine: " + ip);
+			}
+		}
+		function getData() {
+			Computers.all().success(function(data) {
+				vm.computers = data.computers;
+			})
+		}
+
+		vm.refreshData = function() {
+			getData();
+		}
 	});
