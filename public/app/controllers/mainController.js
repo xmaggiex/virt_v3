72d9ed1cc74b53f15a40ed206b7cc1a2c3286479
Ernest Feret <ernest.feret@gmail.com>
2016-05-15 17:43:29 +0100

    active virt list created

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -5,9 +5,11 @@ angular.module('mainController', ['HttpServices'])
         //    vm.test = data
         //});
         getData();
-        vm.line = [{name: "costam", other: "blabla"}];
+        vm.current_computer = {ip: 10, status: "ok"};
+        vm.lines = ['costam', 'inne', 'other'];
+        vm.virt_list = [];
         //vm.status_line = ['danger', 'warning', 'info', 'success'];
-        vm.status_object = {running: 'success', shut: 'default'};
+        vm.status_color = {running: 'success', shut: 'default', ok: 'info', crashed: 'danger'};
         vm.getStatusColor = function(status) {
             return vm.status_object[status];
         };
@@ -16,15 +18,31 @@ angular.module('mainController', ['HttpServices'])
             vm.computers[computerIndex].current_machine_index = virtIndex;
         }
 
+        vm.updateCurrentComputer = function(computerIndex) {
+            vm.current_computer.ip = vm.computers[computerIndex].ip;
+            vm.current_computer.status = vm.computers[computerIndex].status;
+        }
+
         function getData() {
             computerService.getComputers().success(function(data) {
                 vm.computers = data;
                 angular.forEach(vm.computers, function(value, index) {
                     virtualService.getVirtuals(vm.computers[index].ip).success(function(data) {
                         vm.computers[index].virts = data;
+                        for (var i = 0; i < vm.computers[index].virts.length; i++) {
+                            var on_the_list = false;
+                            for (var j = 0; j < vm.virt_list.length; j++) {
+                                if(vm.computers[index].virts[i].name === vm.virt_list[j]) {
+                                    on_the_list = true;
+                                    break;
+                                }
+                            }
+                            if (!on_the_list) {
+                                vm.virt_list.unshift(vm.computers[index].virts[i].name);
+                            }
+                        }
                     });
                 })
-
             });
         };
     }]);
