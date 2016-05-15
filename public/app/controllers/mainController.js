9d84703eddbff98989f2200c19f42ceed76d011c
Ernest Feret <ernest.feret@gmail.com>
2016-05-15 11:37:38 +0100

    main controller with basic bootstrap, ng-change working properly'

--- public/app/controllers/mainController.js
+++ public/app/controllers/mainController.js
@@ -5,6 +5,12 @@ angular.module('mainController', ['HttpServices'])
         //    vm.test = data
         //});
         getData();
+        vm.line = [{name: "costam", other: "blabla"}]
+
+        vm.updateStatus = function(computerIndex, virtIndex) {
+            vm.computers[computerIndex].current_machine_index = virtIndex;
+        }
+
         function getData() {
             computerService.getComputers().success(function(data) {
                 vm.computers = data;
